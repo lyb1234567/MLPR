1f9168f69cb62051e499cbb8327b8f6230606075
Yanbo Liu <yliu431@sheffield.ac.uk>
2022-09-23 10:55:30 +0100

    fix

--- numpy/test.py
+++ numpy/test.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy import array
 from numpy import matrix
-
+import matplotlib.pyplot as plt
 def test_operation():
     print(np.arange(3))
     print(np.arange(3).reshape(1,3))
@@ -26,14 +26,20 @@ def test_operation():
 
     #slice column
     print(m1[:,1])
-
 def lst_square():
     x = np.array([0, 1, 2, 3])
     y = np.array([-1, 0.2, 0.9, 2.1])
     A = np.vstack([x, np.ones(len(x))]).T
+    [k,b]=np.linalg.lstsq(A,y,rcond=None)[0]
+    fitting=[]
+    for i in range(len(x)):
+        fitting.append(x.item(i)*k+b)
+    plt.scatter(x,y,color = 'hotpink',label="scatter")
+    plt.plot(x,fitting,label='fitting line')
+    plt.legend()
+    plt.show()
 if __name__=="__main__":
-    a=np.array([[1,2,3,5,6,7,8,9,10],[1,2,3,5,6,7,8,9,10],[1,2,3,5,6,7,8,9,10]])
-    b=np.matrix('1 2 3 5 6 7 8 9 11')
-    c=np.concatenate([a,b],axis=0)
-    print(c)
+    lst_square()
+
+
 
